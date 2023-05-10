bb71700d0b6e468f808083c9b953be7ff8d0fbb9
Eon4 <ladyforest85@gmail.com>
2023-05-10 13:00:28 +0200

    update

--- assets/js/bus.js
+++ assets/js/bus.js
@@ -3,25 +3,25 @@
         .then(response => response.json())
         .then(jsonData => {
           let departures = jsonData.MultiDepartureBoard.Departure;
-          let limit = 2; // Set the limit to desired departures
+          let limit = 3; // Set the limit to desired departures
           let html = "";
           let counter = 0;
 
           // Iterate over each departure and create HTML elements with desired info
           departures.forEach(departure => {
             if (counter < limit) {
-              html += "<div>" +
-                      "<p>Name: " + departure.name + "</p>" +
-                      "<p>Type: " + departure.type + "</p>" +
-                      "<p>Stop: " + departure.stop + "</p>" +
-                      "<p>Time: " + departure.time + "</p>" +
-                      "<p>Date: " + departure.date + "</p>" +
-                      "<p>ID: " + departure.id + "</p>" +
-                      "<p>Line: " + departure.line + "</p>" +
-                      "<p>Messages: " + departure.messages + "</p>" +
-                      "<p>Final Stop: " + departure.finalStop + "</p>" +
-                      "<p>Direction: " + departure.direction + "</p>" +
-                      "</div>";
+              html += "<br>" +
+                      "<p>Linje: " + departure.line + "</p>" +
+                    //   "<p> " + departure.name + "</p>" +
+                    //   "<p>Type: " + departure.type + "</p>" +
+                      "<p>Stoppested: " + departure.stop + "</p>" +
+                      "<p>Ankomst: " + departure.time + "</p>" +
+                    //   "<p>Date: " + departure.date + "</p>" +
+                    //   "<p>ID: " + departure.id + "</p>" +
+                    //   "<p>Messages: " + departure.messages + "</p>" +
+                      "<p>Destination: " + departure.finalStop + "</p>" +
+                    //   "<p>Direction: " + departure.direction + "</p>" +
+                      "</br>";
               counter++;
             }
           });
