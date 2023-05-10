3437177b00004fcc4ec7e2dae036d38262ac82a5
Eon4 <ladyforest85@gmail.com>
2023-05-10 12:06:14 +0200

    bustider update

---
+++ assets/js/bus.js
@@ -0,0 +1,33 @@
+ // Fetching the JSON file and display the data
+      fetch("../../localfiles/bustider.json")
+        .then(response => response.json())
+        .then(jsonData => {
+          let departures = jsonData.MultiDepartureBoard.Departure;
+          let limit = 2; // Set the limit to desired departures
+          let html = "";
+          let counter = 0;
+
+          // Iterate over each departure and create HTML elements
+          departures.forEach(departure => {
+            if (counter < limit) {
+              html += "<div>" +
+                      "<p>Name: " + departure.name + "</p>" +
+                      "<p>Type: " + departure.type + "</p>" +
+                      "<p>Stop: " + departure.stop + "</p>" +
+                      "<p>Time: " + departure.time + "</p>" +
+                      "<p>Date: " + departure.date + "</p>" +
+                      "<p>ID: " + departure.id + "</p>" +
+                      "<p>Line: " + departure.line + "</p>" +
+                      "<p>Messages: " + departure.messages + "</p>" +
+                      "<p>Final Stop: " + departure.finalStop + "</p>" +
+                      "<p>Direction: " + departure.direction + "</p>" +
+                      "</div>";
+              counter++;
+            }
+          });
+
+          document.getElementById("bus").innerHTML = html;
+        })
+        .catch(error => console.log(error));
+
+        
