744a2e1d6624c39adcc24a2ff094506380888bd3
Eon4 <ladyforest85@gmail.com>
2023-05-10 15:15:24 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-console.log("test");
+console.log("vejret");
 const wrapper = document.getElementById("container-wrapper");
 
 
@@ -40,7 +40,7 @@ let weather = {
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-    //   document.querySelector(".city").innerText = "Vejret i " + name;
+      document.querySelector(".city").innerText = "Vejret i " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
