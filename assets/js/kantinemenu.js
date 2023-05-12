9a1c202b02b0d77d3865aff9cfbb6b54ee3417bb
Eon4 <ladyforest85@gmail.com>
2023-05-12 11:52:17 +0200

    update menu

--- assets/js/kantinemenu.js
+++ assets/js/kantinemenu.js
@@ -1,32 +1,34 @@
 const kantineAPI = "../../localfiles/kantine.json";
 
-console.log("menu");
-
-
 fetch(kantineAPI)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     console.log(data);
-    addkantineToDom(data);
+    addKantineToDom(data);
   })
   .catch((error) => {
     console.log(error);
   });
 
-const activitesElement = document.querySelector("#menu");
+const menuElement = document.querySelector("#menu");
 
-function addkantineToDom(product) {
-  const kantineElement = document.createElement("div");
-  console.log(product);
-  kantineElement.classList.add("item");
+function addKantineToDom(data) {
+  const canteenDays = data.CanteenMenu.Days.CanteenDay;
 
-  kantineElement.innerHTML = `
-    <p>hello
-     </p>
-   
+  canteenDays.forEach((day) => {
+    const kantineItemElement = document.createElement("div");
+    kantineItemElement.classList.add("item");
+
+    kantineItemElement.innerHTML = `
+      <p class="idag"> ${day.DayName}</p>
+      <p class="ret">Ret: ${day.Dish}</p>
+      <p class="uge">Uge: ${data.CanteenMenu.Week}</p>
+      <img class="foodpic"= src="${data.CanteenMenu.PictureUrl}" alt="cantinefoods">
     `;
 
-  activitesElement.appendChild(kantineElement);
+    menuElement.appendChild(kantineItemElement);
+  });
 }
+
