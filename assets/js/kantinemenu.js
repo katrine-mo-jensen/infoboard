df99ec089761e4cc5bb85e94be78c61bd8403193
Eon4 <ladyforest85@gmail.com>
2023-05-12 08:53:41 +0200

    kantine update

--- assets/js/kantinemenu.js
+++ assets/js/kantinemenu.js
@@ -1,1 +1,32 @@
+const kantineAPI = "../../localfiles/kantine.json";
+
 console.log("menu");
+
+
+fetch(kantineAPI)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    addkantineToDom(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const activitesElement = document.querySelector("#menu");
+
+function addkantineToDom(product) {
+  const kantineElement = document.createElement("div");
+  console.log(product);
+  kantineElement.classList.add("item");
+
+  kantineElement.innerHTML = `
+    <p>hello
+     </p>
+   
+    `;
+
+  activitesElement.appendChild(kantineElement);
+}
